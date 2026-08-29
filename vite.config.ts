1a5968c591143548ab204778a837a3c7f4e3806a
GeovaneYu <239038360+GeovaneYu@users.noreply.github.com>
2026-08-29 18:35:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [tailwindcss(), react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/portfolio/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
